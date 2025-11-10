9b402845bdb9129f98fbd5c0a2e20b412c68ec71
Ibai <yvel868@gmail.com>
2025-11-10 11:54:12 +0100

    views modificat i template creat

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
 
 def post_list(request):
-    return HttpResponse("¡Hola Ibai! Bienvenido a tu primer proyecto Django 😎")
+    return render(request, 'blog/post_list.html', {})
